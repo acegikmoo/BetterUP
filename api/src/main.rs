08fd7c396a48915289f472a6a51a81977b493166
rubairakib <rubairakib11@gmail.com>
2025-11-21 16:23:58 +0600

    feat: make db connection thread safe

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,10 @@
+use std::sync::{Arc, Mutex};
+
 use poem::{
-    Route, Server, get, handler,
+    EndpointExt, Route, Server, get, handler,
     listener::TcpListener,
     post,
-    web::{Json, Path},
+    web::{Data, Json, Path},
 };
 
 use crate::{
@@ -14,16 +16,22 @@ pub mod request_inputs;
 pub mod request_outputs;
 
 #[handler]
-fn get_website(Path(id): Path<String>) -> Json<GetWebsiteOutput> {
-    let mut s = Store::new().unwrap();
-    let website = s.get_website(id).unwrap();
+fn get_website(
+    Path(id): Path<String>,
+    Data(s): Data<&Arc<Mutex<Store>>>,
+) -> Json<GetWebsiteOutput> {
+    let mut locked_s = s.lock().unwrap();
+    let website = locked_s.get_website(id).unwrap();
     Json(GetWebsiteOutput { url: website.url })
 }
 
 #[handler]
-fn sign_up(Json(data): Json<CreateUserInput>) -> Json<CreateUserOutput> {
-    let mut s = Store::new().unwrap();
-    let id = s.sign_up(data.username, data.password).unwrap();
+fn sign_up(
+    Json(data): Json<CreateUserInput>,
+    Data(s): Data<&Arc<Mutex<Store>>>,
+) -> Json<CreateUserOutput> {
+    let mut locked_s = s.lock().unwrap();
+    let id = locked_s.sign_up(data.username, data.password).unwrap();
 
     let response = CreateUserOutput { id };
 
@@ -31,9 +39,12 @@ fn sign_up(Json(data): Json<CreateUserInput>) -> Json<CreateUserOutput> {
 }
 
 #[handler]
-fn sign_in(Json(data): Json<CreateUserInput>) -> Json<SigninOutput> {
-    let mut s = Store::new().unwrap();
-    let _exists = s.sign_in(data.username, data.password).unwrap();
+fn sign_in(
+    Json(data): Json<CreateUserInput>,
+    Data(s): Data<&Arc<Mutex<Store>>>,
+) -> Json<SigninOutput> {
+    let mut locked_s = s.lock().unwrap();
+    let _exists = locked_s.sign_in(data.username, data.password).unwrap();
     let response = SigninOutput {
         jwt: String::from("Rubai"),
     };
@@ -41,9 +52,12 @@ fn sign_in(Json(data): Json<CreateUserInput>) -> Json<SigninOutput> {
 }
 
 #[handler]
-fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
-    let mut s = Store::new().unwrap();
-    let website = s
+fn create_website(
+    Json(data): Json<CreateWebsiteInput>,
+    Data(s): Data<&Arc<Mutex<Store>>>,
+) -> Json<CreateWebsiteOutput> {
+    let mut locked_s = s.lock().unwrap();
+    let website = locked_s
         .create_website(
             String::from("ed4b9faf-2bd1-4137-aec3-58eda1f93ae8"),
             data.url,
@@ -53,13 +67,15 @@ fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOut
     Json(response)
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), std::io::Error> {
+    let s = Arc::new(Mutex::new(Store::new().unwrap()));
     let app = Route::new()
         .at("/website/:website_id", get(get_website))
         .at("/website", post(create_website))
         .at("/user/signup", post(sign_up))
-        .at("/user/signin", post(sign_in));
+        .at("/user/signin", post(sign_in))
+        .data(s);
 
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .name("betteruptime-api")
